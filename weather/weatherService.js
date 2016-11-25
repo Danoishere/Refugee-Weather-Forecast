6c751db1edab4c117d6763982a0ac70bae8b7db4
DanoIsHere <dano.roost@gmail.com>
2016-11-25 15:50:33 +0200

    Commit before redesign of Location

--- weather/weatherService.js
+++ weather/weatherService.js
@@ -1,6 +1,7 @@
 var ManageLocations = require('../locations/ManageLocations');
 var WeatherAccess = require('./accessWeatherData');
 var WeatherMetrics = require('./calculateWeatherMetrics');
+var dateFormat = require('dateformat');
 var WeatherService = function(){};
 
 WeatherService.prototype.updateWeatherDataForAllLocations = function(name, countryCode, latitude, longitude){
@@ -9,7 +10,8 @@ WeatherService.prototype.updateWeatherDataForAllLocations = function(name, count
     locations.forEach(function (location) {
       WeatherAccess.loadWeatherForLocation(location, function(loadedLocation, hourlyWeatherData){
         loadedLocation =  WeatherMetrics.createForecastMetrics(loadedLocation,hourlyWeatherData);
-        ManageLocations.saveLocation(location);
+        loadedLocation.updatedAt = dateFormat(new Date(), "dddd, mmmm dS, yyyy, h:MM:ss TT");
+        ManageLocations.saveLocation(loadedLocation);
       });
     });
   } catch (err) {
